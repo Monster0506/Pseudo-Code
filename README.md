bddd7404074c4eafb281cc560d7cec3f2ac8e560
Monster0506 <tjraklovits@gmail.com>
2026-01-14 11:38:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pseudo
+
+
+An implementation of an academic-style pseudo code format in Python
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from psudeo!")
+
+
+if __name__ == "__main__":
+    main()
