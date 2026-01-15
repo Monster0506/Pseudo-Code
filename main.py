6d17261161355b68bde2a8f1289688b9ffcc0ae0
Monster0506 <tjraklovits@gmail.com>
2026-01-15 07:03:16 -0500

    Hook up opcode generation to main flow

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sys import argv
 
 from parser import Parser
 from tokenizer import tokenize, validate_syntax
+from generator import Generator
 
 
 def get_code(filename):
@@ -12,14 +13,19 @@ def get_code(filename):
 
 tokens = tokenize(get_code(argv[1]))
 if not validate_syntax(tokens)[0]:
-    raise SyntaxError(validate_syntax(tokens)[1]) 
+    raise SyntaxError(validate_syntax(tokens)[1])
 
 try:
     parser = Parser(tokens)
     ast = parser.parse()
-    print("AST:", ast)
-    print("Parsed statements:")
-    for stmt in ast.statements:
-        print(f"  {stmt}")
+
+    generator = Generator()
+    instructions = generator.generate(ast)
+    lines = [
+        f"{i:{len(str(len(instructions)))+1}d}: {instr}"
+        for i, instr in enumerate(instructions)
+    ]
+    print("\n".join(lines))
+
 except SyntaxError as e:
     print(f"Parse error: {e}")
